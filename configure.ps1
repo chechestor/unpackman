d75434fbd63c845d44d2fa31e027eb24457d0c8f
che <nuts3000@mail.ru>
2023-02-16 12:26:13 +0600

    mingw32-make instead of make

--- configure.ps1
+++ configure.ps1
@@ -15,19 +15,3 @@ $WebClient.DownloadFile($url,$Arch)
 echo "UNZIP SFML"
 echo "unzip file $Arch"
 Expand-Archive $Arch -DestinationPath ./
-
-#Download mingw tools
-echo "DOWNLOAD MINGW TOOLS"
-$Url = "https://github.com/mstorsjo/llvm-mingw/releases/download/20220906/llvm-mingw-20220906-msvcrt-x86_64.zip"
-$Arch = "$pwd/mingw.zip"
-echo "  Download file from"
-echo "	$Url"
-echo "  to"
-echo "	$Arch"
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile($url,$Arch)
-
-# Unzip MINGW archive
-echo "UNZIP MINGW TOOLS"
-echo "  unzip file $Arch"
-Expand-Archive $Arch -DestinationPath ./
